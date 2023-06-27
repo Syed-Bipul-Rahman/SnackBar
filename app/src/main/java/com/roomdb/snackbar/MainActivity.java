3277690e40c18069ea557c3454a3d0786ad855e0
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-27 20:47:14 +0600

    Snackbar created

--- app/src/main/java/com/roomdb/snackbar/MainActivity.java
+++ app/src/main/java/com/roomdb/snackbar/MainActivity.java
@@ -3,8 +3,12 @@ package com.roomdb.snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
         coordinatorLayout=findViewById(R.id.coordinatorlayout);
         button=findViewById(R.id.snackbarbtn);
 
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar=Snackbar.make(coordinatorLayout,"Snackbar display",Snackbar.LENGTH_INDEFINITE);
+                snackbar.setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Snackbar snackbarundo=Snackbar.make(coordinatorLayout,"Undo Successful",Snackbar.LENGTH_SHORT);
+                    }
+                }).setActionTextColor(Color.GREEN);
+
+                snackbar.show();
+            }
+        });
 
     }
 }
